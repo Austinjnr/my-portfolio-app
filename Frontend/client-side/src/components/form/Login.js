981292b8abb2392a9e5eb08436db44719bd5d8da
austinjnr <austinobonyo@gmail.com>
2023-03-01 14:42:11 +0300

    adds functionality to login form

--- Frontend/client-side/src/components/form/Login.js
+++ Frontend/client-side/src/components/form/Login.js
@@ -1,16 +1,28 @@
+import React, {useState} from 'react';
 
-function login() {
+
+function Login() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(email)
+    }
     return ( 
-        <form>
+        <section>
+        <form onSubmit={handleSubmit}>
             <label for="email">email</label>
-            <input type="email" placeholder="youremail@gmail.com" id="email" name="email"></input>
+            <input value={email} type="email" placeholder="youremail@gmail.com" id="email" name="email"></input>
             
             <lable for="password">password</lable>
-            <input type="password" placeholder="****" id="password" name="password"></input>
+            <input value={password} type="password" placeholder="****" id="password" name="password"></input>
 
-            <button>Log In</button>
+            <button type="submit">Log In</button>
         </form>
+        <button>Don't have an account ? Register</button>
+        </section>
      );
 }
 
-export default login;
+export default Login;
