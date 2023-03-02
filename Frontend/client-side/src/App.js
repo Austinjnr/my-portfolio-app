d2151ebfa08280b395313639fce43fcbe31b806e
austinjnr <austinobonyo@gmail.com>
2023-03-02 22:18:17 +0300

    imports

--- Frontend/client-side/src/App.js
+++ Frontend/client-side/src/App.js
@@ -1,5 +1,8 @@
 import './App.css';
 import Form from './components/form/Form';
+import Navbar from './components/navbar/Navbar';
+import Aboutme from './components/aboutme/Aboutme';
+import 
 
 function App() {
   return (
